220e174cc31b4286c3112b26a913a714bad51882
Taha Husain <taha.husain@weboniselab.com>
2014-08-13 07:38:47 +0530

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 SampleApp::Application.routes.draw do
-  resources :users  
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   get "users/new"
   root 'static_pages#home'
   match '/signup', to: 'users#new', via: [:get]
   match '/help',  to: 'static_pages#help', via: [:get]
   match '/about', to: 'static_pages#about', via: [:get]
   match '/contact', to: 'static_pages#contact', via: [:get]
+  match '/signin', to: 'sessions#new', via: [:get]
+  match '/signout', to: 'sessions#destroy', via: [:delete]
   
   get "static_pages/contact"
   get "static_pages/about"
